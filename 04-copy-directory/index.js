e6b4210e4fcec26825dcc3f776c884634e1e268e
sahion <alexeysuvorov33@gmail.com>
2022-05-22 23:11:44 +0700

    feat: solve task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname,'files-copy'), (error,files)=>{
+  if (files){
+    files.forEach(file => {
+      fs.unlink(path.join(__dirname,'files-copy',file), error => {
+        if (error) console.error(error);
+      });
+    });
+  }
+});
+
+
+fs.mkdir(path.join(__dirname,'files-copy'), {recursive : true}, (error) => {
+  if (error) console.error(error);
+});
+
+
+
+fs.readdir(path.join(__dirname,'files'), (error, files) =>{
+  if (error) console.error(error);
+  files.forEach(file => {
+    fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), error => {
+      if (error) console.error(error);
+    });
+  });
+});
+
+
+
+
 
